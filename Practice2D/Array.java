3d35acb955dd341c089ed7507433890ad9290daf
shrutidalavi205-ship-i <shrutidalavi205@gmail.com>
2026-01-16 14:50:30 +0530

    Practice on 2D Array

---
+++ Practice2D/Array.java
@@ -0,0 +1,62 @@
+package Practice2D;
+
+import java.util.Arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		int[][] num = { { 23, 45, 67, 76, 54 }, { 45, 65, 67, 54, 67 } };
+//Q1.Write a program to declare and initialize a 2D array of integers.
+
+//		for (int i = 0; i < num.length; i++) {
+//
+//			for (int j = 0; j < num[i].length; j++) {
+//
+//				System.out.print(num[i][j] + " ");
+//
+//			}
+//			
+//			System.out.println();
+//		}
+
+//		System.out.println(Arrays.deepToString(num));
+
+//Q2.Write a program to print all elements of a 2D array.
+
+//		for (int i = 0; i < num.length; i++) {
+//			for (int j = 0; j < num[i].length; j++) {
+//				System.out.print(num[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+
+//Q3.Write a program to find the length of a 2D array.
+
+//		System.out.println(num.length);
+
+//Q4.Write a program to find the sum of all elements in a 2D array.
+
+//		int sum = 0;
+//		for (int[] row : num) {
+//			for (int x : row) {
+//				sum = sum + x;
+//			}
+//		}
+//		System.out.println(sum);
+
+//Q5.Write a program to find the maximum element in a 2D array.
+
+//		int max = num[0][0];
+//
+//		for (int i = 0; i < num.length; i++) {
+//			for (int j = 0; j < num[i].length; j++) {
+//				if (num[i][j] > max) {
+//					max = num[i][j];
+//				}
+//			}
+//		}
+//		System.out.println(max);
+
+	}
+}
