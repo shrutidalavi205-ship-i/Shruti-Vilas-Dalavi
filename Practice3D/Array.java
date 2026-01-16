53a1cd97e093f88badc263e4f4d44bad2c689554
shrutidalavi205-ship-i <shrutidalavi205@gmail.com>
2026-01-16 14:51:45 +0530

    Practice on Multi D Array

---
+++ Practice3D/Array.java
@@ -0,0 +1,73 @@
+package Practice3D;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+//Q1.Write a program to declare and initialize a 3D array of integers.
+
+		int[][][] num = { { { 5, 7, 8 }, { 3, 4, 2 }, { 6, 1, 9 } } };
+
+//Q2.Write a program to print all elements of a Multi D array.
+
+//		for (int i = 0; i < num.length; i++) {
+//
+//			for (int j = 0; j < num[i].length; j++) {
+//
+//				for (int k = 0; k < num[i][j].length; k++) {
+//
+//					System.out.print(num[i][j][k] + " ");
+//
+//				}
+//			}
+//
+//		}
+
+//Q3.Write a program to find the length of a Multi D array.
+
+//		for (int i = 0; i < num.length; i++) {
+//
+//			System.out.println(num[i].length);
+//			for (int j = 0; j < num[i].length; j++) {
+//
+//				System.out.println(num[j].length);
+//
+//				for (int k = 0; k < num[i][j].length; k++) {
+//					System.out.println(num[k].length);
+//				}
+//
+//			}
+//		}
+
+//Q4.Write a program to find the maximum element in a Multi D array.
+
+//		int max = num[0][0][0];
+//
+//		for (int i = 0; i < num.length; i++) {
+//			for (int j = 0; j < num[i].length; j++) {
+//				for (int k = 0; k < num[i][j].length; k++) {
+//					if (num[i][j][k] > max) {
+//						max = num[i][j][k];
+//					}
+//				}
+//			}
+//			System.out.println(max);
+//		}
+
+//Q5.	
+
+//		int min = num[0][0][0];
+//
+//		for (int i = 0; i < num.length; i++) {
+//			for (int j = 0; j < num[i].length; j++) {
+//				for (int k = 0; k < num[i][j].length; k++) {
+//					if (num[i][j][k] < min) {
+//						min = num[i][j][k];
+//					}
+//				}
+//			}
+//			System.out.println(min);
+//		}
+
+	}
+}
